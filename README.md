cc35e0ebdbc90f9672f2f8640b64dd2ea9de8fd7
kevingau <kevin.gau@letslinc.com>
2021-02-01 23:57:17 +0800

    add main files

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# CoinGecko-Auto-Candy
-Auto-login to CoinGecko and get daily rewards.
+# CoinGecko
+
+## Summary
+An automated script to get daily rewards from CoinGecko. Thanks to Heroku, the deployment is stress-free and easy. All the deployment is done in one click (almost). 
+
+## Deployment
+
+1. Click the **Deploy to Heroku** Button below
+
+[![Deploy](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy)
+
+2. Setup the `USERNAME` and `PASSWORD` variables
+
+Update the `USERNAME` and `PASSWORD` values with your CoinGecko username and password. 
+
+<img src="https://i.imgur.com/jV8a7fQ.png" width="600px">
+
+
+
+3. Deploy and Build
+
+Hit **Deploy App** and wait until the build is finished. Click `Manage App` redirects to the main page.
+
+<img src="https://i.imgur.com/UnHlu8Q.png" width="600px">
+
+4. Setup Schedules
+
+* Click `Resources` > `Heroku Scheduler` > `Create Job`
+* Choose `Every day at ...` with anytime you like
+* Copy and paste `python main.py` in the command input below
+* Save job
+
+<img src="https://i.imgur.com/hj6adwI.png">
+
+
+5. All set!
+
+## Credit
+
+Inspired by: [CryoliteZ/1point3auto](https://github.com/CryoliteZ/1point3auto)
+
+I found the script for receiving 1point3auto rewards daily and believe that the same concept could be applied on getting CoinGecko's daily checkin rewards.
---
+++ main.py
@@ -0,0 +1,50 @@
+from selenium import webdriver
+import os
+import time
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+driver = webdriver.Chrome('chromedriver')
+
+# Test usage of selenium
+SLEEP_TIME = 5
+
+# Access Site
+try:
+    driver.get('https://www.coingecko.com/account/candy?locale=en')
+    print('Successful Access Site')
+    time.sleep(SLEEP_TIME)
+except BaseException:
+    print('Could not access https://www.coingecko.com/account/candy?locale=en')
+    exit(1001)
+
+# Login
+try:
+    driver.find_element_by_id("user_email").send_keys(os.environ.get("USERNAME"))
+    driver.find_element_by_id("user_password").send_keys(os.environ.get("PASSWORD"))
+    time.sleep(SLEEP_TIME)
+    driver.find_element_by_xpath('//*[@id="new_user"]/input[8]').click()
+    print('Successful Login')
+    time.sleep(SLEEP_TIME)
+except BaseException:
+    print('Cannot login')
+    exit(1002)
+
+# Click Daily Reward
+try:
+    driver.find_element_by_css_selector("body > div.container > div.row.mt-1 > div.col-lg-8.pl-lg-5.mt-lg-5.pt-lg-1 > div:nth-child(3) > div > form > input.btn.btn-primary.col-12.collect-candy-button").click()
+    time.sleep(SLEEP_TIME)
+    print('Successful click Daily Reward')
+except Exception as e:
+    print(e)
+    print('Cannot access daily reward')
+    exit(1003)
+
+# Quit
+driver.quit()
+exit(0)
+
+
